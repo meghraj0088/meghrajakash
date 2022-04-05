0b056981a061e3acb8a5cff9ca157e3f43a09616
meghraj0088 <103029381+meghraj0088@users.noreply.github.com>
2022-04-05 15:39:12 +0600

    Create megh9.py
     pkg update
    
     pkg upgrade
    
    pkg install git
    
    pkg install python3
    
    https://github.com/meghraj0088/meghrajakash.git
    
    cd meghrajakash
    
    python3 megh9.py

---
+++ megh9.py
@@ -0,0 +1,27 @@
+import os, platform
+
+os.system('git pull')
+
+try:
+
+    import requests
+
+except:
+
+    os.system('python3 -m pip install requests')
+
+    os.system('python3 -m pip install bs4')
+
+    os.system('python3 -m pip install futures')
+
+    os.system('python3 -m pip install machine')
+
+    os.system('python3 -m pip install mechanize')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+elif bit == '32bit':
